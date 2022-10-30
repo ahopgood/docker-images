f6aea03927b5bf21e17061180c1981fd2339faa3
Alexander Hopgood <alex.hopgood@gmail.com>
2022-10-30 18:35:19 +0000

    Popping in missing dollar sign

--- getVersionTag.sh
+++ getVersionTag.sh
@@ -6,6 +6,6 @@ TEMP_BRANCH_NAME="$(git rev-parse --abbrev-ref HEAD)"
 if [ "${TEMP_BRANCH_NAME}" == "HEAD" ]; then
     TEMP_BRANCH_NAME="${BRANCH_NAME}"
 fi
-BRANCH_NAME=$(echo TEMP_BRANCH_NAME | sed s/\\//-/)
+BRANCH_NAME=$(echo $TEMP_BRANCH_NAME | sed s/\\//-/)
 
 printf "%s-%s-%s\n" "${BRANCH_NAME}" "${TIMESTAMP}" "${SHORT_SHA}"
